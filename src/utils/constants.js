25342d08a51597b885f17e7dae4d2f5552ea00c4
saimanas <manaskaja@gmail.com>
2025-08-11 17:57:05 +0530

    Remove sensitive file from history and add safe version

---
+++ src/utils/constants.js
@@ -0,0 +1,33 @@
+export const LOGO=
+"https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const USER_AVATAR=
+"https://occ-0-4857-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+
+export const HOME_PAGE_BACKGROUND=
+'https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_large.jpg'
+
+export const PHOTO_URL=
+"https://example.com/jane-q-user/profile.jpg"
+
+export const API_OPTIONS=
+{
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjdiMDk3YzY1MDBlZTU3NTA1ZjRjOGFhMDU2MzRjMyIsIm5iZiI6MTcyNjgwOTg5Ni4xMzUsInN1YiI6IjY2ZWQwNzI4MjVjOGViODg0ZDIyYWFhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k2ErWa-268VdouYyH1a0yd3IDPyy9tw9IcCKUMvqTh4'
+    }
+};
+
+export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500"
+
+export const SUPPORTED_LANGUAGES=[{
+  identifier:"en",name:"English"
+},{
+  identifier:"hindi",name:"Hindi"
+},{
+  identifier:"spanish",name:"Spanish"
+}]
+
+// Please uncomment and use your own OPENAI_API_KEY to perform operations
+// export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
